d721ae41bc26e026e4e808222c1d1cdfd3c4348c
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2021-09-13 02:06:59 +1200

    heroku

--- app.js
+++ app.js
@@ -12,11 +12,14 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 
+// Accessing the path module
 const path = require("path");
 
+// Step 1:
 app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+// Step 2:
 app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
