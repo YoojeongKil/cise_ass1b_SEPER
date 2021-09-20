5203a9dc90c916b62233f6a90eed2efbb542fe8e
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2021-09-21 00:27:21 +1200

    Creating models

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ require("dotenv").config({path: "./config.env"});
 
 const express = require('express');
 const connectDB = require('./config/db');
+//var cors = require('cors');
 
 const app = express();
 
 // Connect Database
 connectDB();
+
+//cors
+//app.use(cors({origin: true, credentials: true}));
 
 //app.get('/', (req, res) => res.send('Hello world!'));
 
