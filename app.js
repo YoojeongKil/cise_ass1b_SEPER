2df767ed8698d0bd052d745e8db9a872b81d6a2f
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2021-09-24 16:31:49 +1200

    Fixing app.js

--- app.js
+++ app.js
@@ -28,11 +28,12 @@ const port = process.env.PORT || 5000;
 // Accessing the path module
 const path = require("path");
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-});
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+  
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
