5c7b71ac94695f07a494c32c4e8ef593dc4d3b0d
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2021-10-09 01:20:41 +1300

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div class="welcomesign"> 
+      <div className="welcomesign"> 
         <h2> Welcome to SEPER</h2>
-        <div class="maincontent">
+        <div className="maincontent">
           <p>Begin your search now. The Software Practice Empirical Evidence 
             Database allows you to search for recommended articles about a specific 
             Software Engineering practice. </p>
-          <div class="searchbar">
+          <div className="searchbar">
             <h4>Begin your first search:</h4>
             <br></br>
             <input type="text" placeholder="Enter search"></input>
